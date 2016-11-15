185d75134630dfa52e80b128b33b2bf65d3eecf0
miewsukanya <miewsukanya.24@gmail.com>
2016-11-15 22:39:40 +0700

    Create MainActivity Intent Finish

---
+++ app/src/main/java/miewsukanya/com/findsign/MainActivity.java
@@ -0,0 +1,52 @@
+package miewsukanya.com.findsign;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    //Explicit
+
+    private ImageView searchQuickImageView,searchSignImageView, knowLedgeImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Bind widget
+        searchQuickImageView = (ImageView) findViewById(R.id.SearchSignQuick);
+        searchSignImageView = (ImageView) findViewById(R.id.SearchSign);
+        knowLedgeImageView = (ImageView) findViewById(R.id.Knowledge);
+
+
+        //search sign controller
+        searchSignImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,SearchSign.class));
+
+            }
+        });
+        //search quick controller
+        searchQuickImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,SearchQuick.class));
+            }
+        });
+
+        //knowledge controller
+        knowLedgeImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,Knowledge.class));
+            }
+        });
+
+    }//Main Method
+
+
+}//Main Class
