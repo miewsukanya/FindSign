66f0bdda659cc5f6d02977ee2dd46307791337b5
miewsukanya <miewsukanya.24@gmail.com>
2016-11-17 03:36:34 +0700

    Finish 17/11/2016

---
+++ app/src/main/java/miewsukanya/com/findsign/SelectTypeSearch.java
@@ -0,0 +1,13 @@
+package miewsukanya.com.findsign;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectTypeSearch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_type_search);
+    }
+}
