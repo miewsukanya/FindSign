9840804f5588b2fcc952f0b2746dc6c673dd661d
miewsukanya <miewsukanya.24@gmail.com>
2016-11-17 00:10:19 +0700

    Create ARView

---
+++ app/src/main/java/miewsukanya/com/findsign/OnAzimuthChangedListener.java
@@ -0,0 +1,8 @@
+package miewsukanya.com.findsign;
+
+/**
+ * Created by krzysztofjackowski on 24/09/15.
+ */
+public interface OnAzimuthChangedListener {
+    void onAzimuthChanged(float azimuthFrom, float azimuthTo);
+}
---
+++ app/src/main/java/miewsukanya/com/findsign/OnLocationChangedListener.java
@@ -0,0 +1,10 @@
+package miewsukanya.com.findsign;
+
+import android.location.Location;
+
+/**
+ * Created by krzysztofjackowski on 24/09/15.
+ */
+public interface OnLocationChangedListener {
+    void onLocationChanged(Location currentLocation);
+}
