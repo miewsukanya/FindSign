25ec2d77e36c0a30db55eb3e29834f67ed692f09
miewsukanya <miewsukanya.24@gmail.com>
2016-11-17 03:03:20 +0700

    Create MapSearch

---
+++ app/src/main/java/miewsukanya/com/findsign/MapSearch.java
@@ -0,0 +1,13 @@
+package miewsukanya.com.findsign;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MapSearch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_search);
+    }
+}
