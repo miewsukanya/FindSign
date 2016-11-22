8dd2338e7d5d1f764a0d53fbb3cc07d10e0d4682
miewsukanya <miewsukanya.24@gmail.com>
2016-11-22 22:54:02 +0700

    Create MapSearch Maker

--- app/src/main/java/miewsukanya/com/findsign/MapSearch.java
+++ app/src/main/java/miewsukanya/com/findsign/MapSearch.java
@@ -1,13 +1,75 @@
 package miewsukanya.com.findsign;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
-public class MapSearch extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapSearch extends AppCompatActivity implements OnMapReadyCallback {
+
+    GoogleMap mGoogleMap;
+    GoogleApiClient mGoogleClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map_search);
+        if (googleServicesAvailable()) {
+            Toast.makeText(this, "Perfect!!", Toast.LENGTH_LONG).show();
+            setContentView(R.layout.activity_map_search);
+            initMap();
+
+        } else {
+            //No google map layout
+        }
+    }//Main Method
+
+    //Show map
+    private void initMap() {
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapFragment);
+        mapFragment.getMapAsync(this);
+
     }
-}
+    public boolean googleServicesAvailable() {
+        GoogleApiAvailability api = GoogleApiAvailability.getInstance();
+        int isAvailable = api.isGooglePlayServicesAvailable(this);
+        if (isAvailable == ConnectionResult.SUCCESS) {
+            return true;
+        } else if (api.isUserResolvableError(isAvailable)) {
+            Dialog dialog = api.getErrorDialog(this, isAvailable, 0);
+            dialog.show();
+        } else {
+            Toast.makeText(this, "Cant connect to play services", Toast.LENGTH_LONG).show();
+        }
+        return false;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mGoogleMap = googleMap;
+        LatLng comsci = new LatLng(16.474475, 102.82313);
+        mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mGoogleMap.addMarker(new MarkerOptions()
+                .position(comsci)
+                .title("comsci"));
+        //  แพนเลื่อนแผนที่ไปพิกัดที่ระบุ
+        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(comsci));
+        goToLocationZoom(16.474475, 102.82313,15);
+    }
+    //class zoom map
+    private void goToLocationZoom(double lat, double lng, float zoom) {
+        LatLng latlng = new LatLng(lat, lng);
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(latlng, zoom);
+        mGoogleMap.moveCamera(update);
+    }
+}//Main Class
--- app/src/main/java/miewsukanya/com/findsign/SelectTypeSearch.java
+++ app/src/main/java/miewsukanya/com/findsign/SelectTypeSearch.java
@@ -1,13 +1,36 @@
 package miewsukanya.com.findsign;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class SelectTypeSearch extends AppCompatActivity {
+    //Explicit
+    ImageView ShowARImageView, ShowMapImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_type_search);
+
+        //BindWidGet
+        ShowARImageView = (ImageView) findViewById(R.id.ShowAR);
+        ShowMapImageView = (ImageView) findViewById(R.id.ShowMap);
+
+        //showARImageViewController
+        ShowARImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SelectTypeSearch.this,ArViewActivity.class));
+            }
+        });
+        ShowMapImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SelectTypeSearch.this,MapSearch.class));
+            }
+        });
     }
 }
