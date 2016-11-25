91e6170441b4a328d1002663ababd9970320a8c7
miewsukanya <miewsukanya.24@gmail.com>
2016-11-26 04:07:27 +0700

    new MapSearch

--- app/src/main/java/miewsukanya/com/findsign/MapSearch.java
+++ app/src/main/java/miewsukanya/com/findsign/MapSearch.java
@@ -1,7 +1,6 @@
 package miewsukanya.com.findsign;
 
 import android.app.Dialog;
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -22,24 +21,15 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
-import com.squareup.okhttp.Response;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MapSearch extends AppCompatActivity implements OnMapReadyCallback {
-    private static final String TAG = "myapp";
     //Explicit
-    //private String[] SignID, SignName,Latitude,Longitude;
-    private  String[] SignIDStrings, SignNameStrings,LatitudeStrings,LongitudeStrings;
-    //private Double Latitude = 0.00;
-    //private Double Longitude = 0.00;
-    //ArrayList<HashMap<String, String>> location = null;
     GoogleMap mGoogleMap;
     GoogleApiClient mGoogleClient;
-    TextView textJsonTextView;
-    ProgressDialog progressDialog;
+    TextView txtJson;
     private MyConstant myConstant;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,25 +37,62 @@ public class MapSearch extends AppCompatActivity implements OnMapReadyCallback {
         if (googleServicesAvailable()) {
             Toast.makeText(this, "Perfect!!", Toast.LENGTH_LONG).show();
             setContentView(R.layout.activity_map_search);
-            initMap();
-           // getJson();
+            //getjson();
             myConstant = new MyConstant();
-            getJson getJson = new getJson(MapSearch.this);
-            getJson.execute(myConstant.getUrlGetJSON());
-
-
+            SynUser synUser = new SynUser(MapSearch.this);
+            synUser.execute(myConstant.getUrlGetJSON());
+            initMap();
 
         } else {
             //No google map layout
         }
     }//Main Method
-    private class getJson extends AsyncTask<String, Void, String> {
+
+    /*public void getjson() {
+        txtJson = (TextView) findViewById(R.id.textJson);
+        String JsonUrl = "http://202.28.94.32/2559/563020232-9/getlatlong.php";
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(JsonUrl, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                JSONObject jsonObject;
+
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+
+                        jsonObject = response.getJSONObject(i);
+                        int SignID = jsonObject.getInt("SignID");
+                        String SignName = jsonObject.getString("SignName");
+                        String Latitude = jsonObject.getString("Latitude");
+                        String Longitude = jsonObject.getString("Longitude");
+
+                        Log.d("Json", SignID + "," + SignName + "," + Latitude + "," + Longitude);
+
+                        txtJson.setText(SignID+"\n"+SignName+"\n"+Latitude+"\n"+Longitude+"\n");
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        txtJson.setText("onErrorRespone():"+error.getMessage());
+                    }
+                }
+        );
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(jsonArrayRequest);
+    }//getJson*/
+
+    private class SynUser extends AsyncTask<String, Void, String> {
         //Explicit
         private Context context;
-        private String[] SignIDStrings,SignNameStrings,LatitudeStrings, LongitudeStrings;
-        //private Boolean aBoolean = true;
+        private String[] SingNameStrings,LatitudeStrings, LongitudeStrings;
+        private int[] SingIDInts;
 
-        public getJson(Context context) {
+        public SynUser(Context context) {
             this.context = context;
         }
         @Override
@@ -73,62 +100,56 @@ public class MapSearch extends AppCompatActivity implements OnMapReadyCallback {
             try {
                 OkHttpClient okHttpClient = new OkHttpClient();
                 Request.Builder builder = new Request.Builder();
-                Request request  = builder.url(params[0]).build();
-                Response response = okHttpClient.newCall(request).execute();
+                Request request = builder.url(params[0]).build();
+                com.squareup.okhttp.Response response = okHttpClient.newCall(request).execute();
                 return response.body().string();
 
             } catch (Exception e) {
-                Log.d("24novV1", "e doIn ==>" + e.toString());
+                Log.d("26novV1", "e doIn==>" + e.toString());
                 return null;
             }
+            //return null;
         }//doInBack
-
         @Override
         protected void onPostExecute(String s) {
 
-            Log.d("24novV1", "Json ==>" + s);
+            Log.d("26novV1", "Json ==>" + s);
             try {
                 JSONArray jsonArray = new JSONArray(s);
-                /*location = new ArrayList<HashMap<String, String>>();
-                HashMap<String, String> map;*/
-                SignIDStrings = new String[jsonArray.length()];
-                SignNameStrings = new String[jsonArray.length()];
+
+                SingIDInts = new int[jsonArray.length()];
+                SingNameStrings = new String[jsonArray.length()];
                 LatitudeStrings = new String[jsonArray.length()];
                 LongitudeStrings = new String[jsonArray.length()];
-
-                for (int i=0;i<jsonArray.length();i++){
+                for (int i=0;i<jsonArray.length();i++) {
                     JSONObject jsonObject = jsonArray.getJSONObject(i);
 
-                    SignIDStrings[i] = jsonObject.getString("SignID");
-                    SignNameStrings[i] = jsonObject.getString("SignName");
+                    SingIDInts[i] = jsonObject.getInt("SignID");
+                    SingNameStrings[i] = jsonObject.getString("SignName");
                     LatitudeStrings[i] = jsonObject.getString("Latitude");
                     LongitudeStrings[i] = jsonObject.getString("Longitude");
 
-                    /*map = new HashMap<String, String>();
-                    map.put("SignID", jsonObject.getString("SignID"));
-                    map.put("SignName", jsonObject.getString("SignName"));
-                    map.put("Latitude", jsonObject.getString("Latitude"));
-                    map.put("Longitude", jsonObject.getString("Longitude"));
-                    location.add(map);*/
 
-                    Log.d("24novV1", "Latitude (" + LatitudeStrings[i] + ")" + ",(" + LongitudeStrings[i] + ")");
+                    txtJson = (TextView) findViewById(R.id.textJson);
+                    txtJson.setText(+SingIDInts[i]+"\n");
 
+                    Log.d("26novV1", "name(" + i + ") ==>" + SingIDInts[i]);
 
-                }
-
-            } catch (JSONException e) {
+                }//for
+            } catch (Exception e) {
                 e.printStackTrace();
             }
+            //super.onPostExecute(s);
 
-        }
-    }//get json
+        }//onPost
+    }//SnyUser
 
     //Show map
     private void initMap() {
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapFragment);
         mapFragment.getMapAsync(this);
+    }//initMap
 
-    }
     public boolean googleServicesAvailable() {
         GoogleApiAvailability api = GoogleApiAvailability.getInstance();
         int isAvailable = api.isGooglePlayServicesAvailable(this);
@@ -141,43 +162,25 @@ public class MapSearch extends AppCompatActivity implements OnMapReadyCallback {
             Toast.makeText(this, "Cant connect to play services", Toast.LENGTH_LONG).show();
         }
         return false;
-    }//google services
+    }//googleServicesAvailable
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mGoogleMap = googleMap;
-
-        /*Latitude = Double.parseDouble(location.get(0).get("Latitude").toString());
-        Longitude = Double.parseDouble(location.get(0).get("Longitude").toString());
-        LatLng coordinate = new LatLng(Latitude, Longitude);
-        mGoogleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(coordinate, 17));*/
         mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-        //latlng sc06
-       // LatLng comsci = new LatLng(lat, lng);
-        LatLng drom8 = new LatLng(16.4795142, 102.809175);
-
+        LatLng comsci = new LatLng(16.4795142,102.809175);
         mGoogleMap.addMarker(new MarkerOptions()
-                    .position(drom8)
-                    .title("comsci"));
-            //  แพนเลื่อนแผนที่ไปพิกัดที่ระบุ
-        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(drom8));
+                .position(comsci)
+                .title("comsci"));
+        //  แพนเลื่อนแผนที่ไปพิกัดที่ระบุ
+        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(comsci));
         goToLocationZoom(16.4795142, 102.809175, 15);
-        /*for (int i = 0; i < location.size(); i++) {
-            Latitude = Double.parseDouble(location.get(i).get("Latitude").toString());
-            Longitude = Double.parseDouble(location.get(i).get("Longitude").toString());
-            String name = location.get(i).get("SignName").toString();
-            mGoogleMap.addMarker(new MarkerOptions()
-                    .position(new LatLng(Latitude, Longitude))
-                    .title(name));
-            //mGoogleMap.addMarker(marker);
-        }*/
     }//onMapReady
 
-
-    //class zoom map
     private void goToLocationZoom(double lat, double lng, float zoom) {
         LatLng latlng = new LatLng(lat, lng);
         CameraUpdate update = CameraUpdateFactory.newLatLngZoom(latlng, zoom);
         mGoogleMap.moveCamera(update);
-    }
+    }//class zoom map
+
 }//Main Class
