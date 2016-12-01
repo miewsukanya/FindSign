601a15702837fafc020ab1914fc603b51aed87d8
miewsukanya <miewsukanya.24@gmail.com>
2016-12-01 23:35:07 +0700

    Edit Knowledge Page Success

--- app/src/main/java/miewsukanya/com/findsign/Knowledge.java
+++ app/src/main/java/miewsukanya/com/findsign/Knowledge.java
@@ -1,13 +1,18 @@
 package miewsukanya.com.findsign;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Knowledge extends AppCompatActivity {
-
+    ViewPager viewPager;
+    SwipeAdapter swipeAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_knowledge);
+        viewPager = (ViewPager) findViewById(R.id.view_paper);
+        swipeAdapter = new SwipeAdapter(this);
+        viewPager.setAdapter(swipeAdapter);
     }
 }
