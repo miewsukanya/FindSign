64c129a6e7d7b8e384091d799a17a13684b4ebcc
miewsukanya <miewsukanya.24@gmail.com>
2016-12-01 23:36:06 +0700

    Edit SwipeAdapter plus Knowledge Page

--- app/src/main/java/miewsukanya/com/findsign/SwipeAdapter.java
+++ app/src/main/java/miewsukanya/com/findsign/SwipeAdapter.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 /**
  * Created by Sukanya Boonpun on 26/11/2559.
@@ -17,7 +16,7 @@ import android.widget.TextView;
 public class SwipeAdapter extends PagerAdapter{
     private int[] image_resource = new int[] {R.drawable.kn_0,
             R.drawable.kn_1, R.drawable.kn_2,
-            R.drawable.kn_3, R.drawable.kn_4
+            R.drawable.kn_3, R.drawable.kn_4,R.drawable.kn_5
             };
     private Context context;
     private LayoutInflater layoutInflater;
@@ -40,9 +39,9 @@ public class SwipeAdapter extends PagerAdapter{
         layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View view = layoutInflater.inflate(R.layout.swipe_layout, container, false);
         ImageView imageView = (ImageView) view.findViewById(R.id.image_view);
-        TextView textView = (TextView) view.findViewById(R.id.image_count);
+       // TextView textView = (TextView) view.findViewById(R.id.image_count);
         imageView.setImageResource(image_resource[position]);
-        textView.setText(""+position);
+       // textView.setText(""+position);
         container.addView(view);
         Log.d("01DecV1", String.valueOf(position));
         return view;
