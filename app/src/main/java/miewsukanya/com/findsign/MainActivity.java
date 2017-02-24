ccfd7a01dafa53643730e12e5c153dc272e2aaaa
miewsukanya <miewsukanya.24@gmail.com>
2017-02-25 00:54:35 +0700

    25/02/17 แก้ไขหน้า Setting แก้ไขหน้าตั้งค่าใหม่ ต้องส่งค่าไปหน้าค้นหาป้าย

--- app/src/main/java/miewsukanya/com/findsign/MainActivity.java
+++ app/src/main/java/miewsukanya/com/findsign/MainActivity.java
@@ -3,19 +3,17 @@ package miewsukanya.com.findsign;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.ContextMenu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     //Explicit
 
     private ImageView searchQuickImageView,searchSignImageView, knowLedgeImageView,btn_setting;
-
+    TextView txtSetting;
+    int idSettingSelected = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,15 +24,16 @@ public class MainActivity extends AppCompatActivity {
         searchSignImageView = (ImageView) findViewById(R.id.SearchSign);
         knowLedgeImageView = (ImageView) findViewById(R.id.Knowledge);
         btn_setting = (ImageView) findViewById(R.id.btn_setting);
+        txtSetting = (TextView) findViewById(R.id.txtSetting);
 
         //setting controller
         btn_setting.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                registerForContextMenu(v);
-                openContextMenu(v);
-                //startActivity(new Intent(MainActivity.this,SettingActivity.class));
+               // registerForContextMenu(v);
+               // openContextMenu(v);
+                startActivity(new Intent(MainActivity.this,SettingActivity.class));
 
             }
         });
@@ -67,12 +66,44 @@ public class MainActivity extends AppCompatActivity {
 
     }//Main Method
 
-    @Override
+    /*@Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu,v,menuInfo);
 
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.setting_menu,menu);
+
+        MenuItem selectedRad300_45 = menu.findItem(R.id.distance300sign45);
+        MenuItem selectedRad300_60 = menu.findItem(R.id.distance300sign60);
+        MenuItem selectedRad300_80 = menu.findItem(R.id.distance300sign80);
+
+        MenuItem selectedRad400_45 = menu.findItem(R.id.distance400sign45);
+        MenuItem selectedRad400_60 = menu.findItem(R.id.distance400sign60);
+        MenuItem selectedRad400_80 = menu.findItem(R.id.distance400sign80);
+
+        MenuItem selectedRad500_45 = menu.findItem(R.id.distance500sign45);
+        MenuItem selectedRad500_60 = menu.findItem(R.id.distance500sign60);
+        MenuItem selectedRad500_80 = menu.findItem(R.id.distance500sign80);
+
+        if (idSettingSelected == 1) {
+            selectedRad300_45.setChecked(true);
+        } else if (idSettingSelected == 2) {
+            selectedRad300_60.setChecked(true);
+        } else if (idSettingSelected == 3) {
+            selectedRad300_80.setChecked(true);
+        }else if (idSettingSelected == 4) {
+            selectedRad400_45.setChecked(true);
+        }else if (idSettingSelected == 5) {
+            selectedRad400_60.setChecked(true);
+        }else if (idSettingSelected == 6) {
+            selectedRad400_80.setChecked(true);
+        }else if (idSettingSelected == 7) {
+            selectedRad500_45.setChecked(true);
+        }else if (idSettingSelected == 8) {
+            selectedRad500_60.setChecked(true);
+        }else if (idSettingSelected == 9) {
+            selectedRad500_80.setChecked(true);
+        }
 
     }//onCreateContextMenu
 
@@ -80,20 +111,64 @@ public class MainActivity extends AppCompatActivity {
     public boolean onContextItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.distance300sign45:
-                String idSetting = "1";
-                Intent intent = new Intent(getApplicationContext(), MapSearch.class);
-                intent.putExtra("idSetting", idSetting);
-                startActivity(intent);
-                finish();
-                Log.d("24FebV1","Select idSetting :"+ idSetting);
+               // String idSetting = "1";
+                txtSetting.setText("1");
+                item.setChecked(true);
+                idSettingSelected = 1;
+                Log.d("24FebV1","Select idSetting :"+ txtSetting.toString());
+                return true;
+            case R.id.distance300sign60:
+                txtSetting.setText("2");
+                item.setChecked(true);
+                idSettingSelected = 2;
+               // Log.d("24FebV2","Select idSetting :"+ idSetting);
+                return true;
+            case R.id.distance300sign80:
+                txtSetting.setText("3");
+                item.setChecked(true);
+                idSettingSelected = 3;
+                // Log.d("24FebV2","Select idSetting :"+ idSetting);
+                return true;
 
             case R.id.distance400sign45:
-                idSetting = "2";
-                intent = new Intent(getApplicationContext(), MapSearch.class);
-                intent.putExtra("idSetting", idSetting);
-                Log.d("24FebV2","Select idSetting :"+ idSetting);
+                txtSetting.setText("4");
+                item.setChecked(true);
+                idSettingSelected = 4;
+                // Log.d("24FebV2","Select idSetting :"+ idSetting);
+                return true;
+            case R.id.distance400sign60:
+                txtSetting.setText("5");
+                item.setChecked(true);
+                idSettingSelected = 5;
+                // Log.d("24FebV2","Select idSetting :"+ idSetting);
+                return true;
+            case R.id.distance400sign80:
+                txtSetting.setText("6");
+                item.setChecked(true);
+                idSettingSelected = 6;
+                // Log.d("24FebV2","Select idSetting :"+ idSetting);
+                return true;
 
+            case R.id.distance500sign45:
+                txtSetting.setText("7");
+                item.setChecked(true);
+                idSettingSelected = 7;
+                // Log.d("24FebV2","Select idSetting :"+ idSetting);
+                return true;
+            case R.id.distance500sign60:
+                txtSetting.setText("8");
+                item.setChecked(true);
+                idSettingSelected = 8;
+                // Log.d("24FebV2","Select idSetting :"+ idSetting);
+                return true;
+            case R.id.distance500sign80:
+                txtSetting.setText("9");
+                item.setChecked(true);
+                idSettingSelected = 9;
+                // Log.d("24FebV2","Select idSetting :"+ idSetting);
+                return true;
         }
         return super.onContextItemSelected(item);
-    }
+    }//*/
+
 }//Main Class
--- app/src/main/java/miewsukanya/com/findsign/SettingActivity.java
+++ app/src/main/java/miewsukanya/com/findsign/SettingActivity.java
@@ -5,88 +5,160 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.ContextMenu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class SettingActivity extends AppCompatActivity {
 
     //Explicit
-    //RadioGroup RadioGroupDistance;
-   // RadioButton RadioButtonDistance;
-    TextView textViewDistance;
+    RadioGroup RadioGroupDistance;
+    RadioButton rad300,rad400,rad500,rad45,rad60,rad80;
+    TextView textViewDistance,textViewSign;
     int distanceSelected = 0;
+    int signSelected = 0;
+    Button btn_saveSetting;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
 
         textViewDistance = (TextView) findViewById(R.id.txtDistanceSetting);
+        textViewSign = (TextView) findViewById(R.id.txtSignSetting);
+        btn_saveSetting = (Button) findViewById(R.id.btn_saveSetting);
+        rad300 = (RadioButton) findViewById(R.id.rad300);
+        rad400 = (RadioButton) findViewById(R.id.rad400);
+        rad500 = (RadioButton) findViewById(R.id.rad500);
 
-        textViewDistance.setEnabled(false);
+        rad45 = (RadioButton) findViewById(R.id.radioButton);
+        rad60 = (RadioButton) findViewById(R.id.radioButton2);
+        rad80 = (RadioButton) findViewById(R.id.radioButton3);
+
+
+        textViewSign.setText("1");
+        textViewDistance.setText("1");
+        textViewDistance.setEnabled(true);
+        textViewSign.setEnabled(true);
+
+
+
+        btn_saveSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int idSignSetting, idDistanceSetting;
+                idSignSetting = Integer.parseInt(textViewSign.getText().toString());
+                idDistanceSetting = Integer.parseInt(textViewDistance.getText().toString());
+
+                if (idSignSetting == 1 && idDistanceSetting == 1) {
+                    rad45.setChecked(true);
+                    rad300.setChecked(true);
+
+
+                } else if (idSignSetting == 1 && idDistanceSetting == 2) {
+                    rad45.setChecked(true);
+                    rad400.setChecked(true);
+
+
+                } else if (idSignSetting == 1 && idDistanceSetting == 3) {
+                    rad45.setChecked(true);
+                    rad500.setChecked(true);
+
+
+                } else if (idSignSetting == 2 && idDistanceSetting == 1) {
+                    rad60.setChecked(true);
+                    rad300.setChecked(true);
+
+
+                } else if (idSignSetting == 2 && idDistanceSetting == 2) {
+                    rad60.setChecked(true);
+                    rad400.setChecked(true);
+
+                } else if (idSignSetting == 2 && idDistanceSetting == 3) {
+                    rad60.setChecked(true);
+                    rad500.setChecked(true);
+
+                } else if (idSignSetting == 3 && idDistanceSetting == 1) {
+                    rad80.setChecked(true);
+                    rad300.setChecked(true);
+
+                } else if (idSignSetting == 3 && idDistanceSetting == 2) {
+                    rad80.setChecked(true);
+                    rad400.setChecked(true);
+
+                } else if (idSignSetting == 3 && idDistanceSetting == 3) {
+                    rad80.setChecked(true);
+                    rad500.setChecked(true);
+                }
+            }//onClick
+        });//btn_saveSetting
     }//main method
+    public void onSign(View view) {
+        boolean check = ((RadioButton) view).isChecked();
 
+        switch (view.getId()) {
+            case R.id.radioButton:
+                if (check) {
+                    textViewSign.setText("1");
+                    textViewSign.setEnabled(true);
+                    distanceSelected = 1;
+                } else {
+                    textViewSign.setEnabled(false);
+                }
+                break;
+            case R.id.radioButton2:
+                if (check) {
+                    textViewSign.setText("2");
+                    textViewSign.setEnabled(true);
+                    distanceSelected = 2;
+                } else {
+                    textViewSign.setEnabled(false);
+                }
+                break;
+            case R.id.radioButton3:
+                if (check) {
+                    textViewSign.setText("3");
+                    textViewSign.setEnabled(true);
+                    distanceSelected = 3;
+                } else {
+                    textViewSign.setEnabled(false);
+                }
+                break;
+        }//switch case
+    }//onSign*/
     public void onDistance(View view) {
         boolean check = ((RadioButton) view).isChecked();
 
         switch (view.getId()) {
             case R.id.rad300:
                 if (check) {
-                    textViewDistance.setText("0.30");
+                    textViewDistance.setText("1");
                     textViewDistance.setEnabled(true);
-                   distanceSelected = 1;
+                   // signSelected = 1;
                 } else {
                     textViewDistance.setEnabled(false);
                 }
             break;
             case R.id.rad400:
                 if (check) {
-                    textViewDistance.setText("0.40");
-                  //  textViewDistance.setEnabled(true);
-                   distanceSelected = 2;
+                    textViewDistance.setText("2");
+                    textViewDistance.setEnabled(true);
+                    //signSelected = 2;
                 } else {
                     textViewDistance.setEnabled(false);
                 }
                 break;
             case R.id.rad500:
                 if (check) {
-                    textViewDistance.setText("0.50");
-                  //  textViewDistance.setEnabled(true);
-                   distanceSelected = 3;
+                    textViewDistance.setText("3");
+                    textViewDistance.setEnabled(true);
+                    signSelected = 3;
                 } else {
                     textViewDistance.setEnabled(false);
                 }
                 break;
         }//switch case
     }//onDistance*/
-   /* public void onDistance(View view) {
-        registerForContextMenu(view);
-        openContextMenu(view);
-    }//*/
-    /*@Override
-    public boolean onContextItemSelected(MenuItem item) {
-
-        switch (item.getItemId()) {
-            case R.id.rad300:
-
-                    textViewDistance.setText("0.30");
-                    item.setChecked(true);
-                    distanceSelected = 1;
-                    return true;
-
-            case R.id.rad400:
-                    textViewDistance.setText("0.40");
-                    item.setChecked(true);
-                    distanceSelected = 2;
-                    return true;
-
-            case R.id.rad500:
-                    textViewDistance.setText("0.50");
-                    item.setChecked(true);
-                    distanceSelected = 3;
-                    return true;
-        }
-        return super.onContextItemSelected(item);
-    }//*/
 
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
@@ -96,6 +168,10 @@ public class SettingActivity extends AppCompatActivity {
         MenuItem selectedRad400 = menu.findItem(R.id.rad400);
         MenuItem selectedRad500 = menu.findItem(R.id.rad500);
 
+        MenuItem selectedSign45 = menu.findItem(R.id.radioButton);
+        MenuItem selectedSign60 = menu.findItem(R.id.radioButton2);
+        MenuItem selectedSign80 = menu.findItem(R.id.radioButton3);
+
         if (distanceSelected == 1) {
             selectedRad300.setChecked(true);
         } else if (distanceSelected == 2) {
@@ -103,5 +179,18 @@ public class SettingActivity extends AppCompatActivity {
         } else if (distanceSelected == 3) {
             selectedRad500.setChecked(true);
         }
+
+        if (signSelected == 1) {
+            selectedSign45.setChecked(true);
+        } else if (signSelected == 2) {
+            selectedSign60.setChecked(true);
+        } else if (signSelected == 3) {
+            selectedSign80.setChecked(true);
+        }
     }//onCreateContextMenu*/
+
+    public void onSaveSetting(View view) {
+
+
+    }//onSaveSetting
 }//main class
